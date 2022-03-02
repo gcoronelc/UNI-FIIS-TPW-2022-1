300fdc065d74d32483a60480ab94c36b687a1f64
Gustavo Coronel <gcoronelc@gmail.com>
2022-03-02 11:32:47 -0500

    Semana 03

---
+++ Semana03/proyecto.js
@@ -0,0 +1,46 @@
+function fnCalcularSuma() {
+  hacerVisible("calcularSuma");
+}
+
+function fnCalcularVenta() {
+  hacerVisible("calcularVenta");
+}
+
+function hacerVisible(id) {
+  // Oculta todos
+  document.getElementById("calcularSuma").style.display = "none";
+  document.getElementById("calcularVenta").style.display = "none";
+  document.getElementById("encontrarMayor").style.display = "none";
+  document.getElementById("factorial").style.display = "none";
+  document.getElementById("tablaMultiplicar").style.display = "none";
+  // Hacer visible el id
+  document.getElementById(id).style.display = "block";
+}
+
+/* Boton calcular suma */
+document.getElementById("btnCalcularSuma").onclick = function () {
+  // Datos
+  let num1 = parseInt(document.getElementById("num1").value);
+  let num2 = parseInt(document.getElementById("num2").value);
+  // Proceso
+  let suma = num1 + num2;
+  // Reporte
+  document.getElementById("suma").value = suma;
+};
+
+/* Botón procesar venta */
+document.getElementById("btnProcesarVenta").onclick = function () {
+  // Variables
+  let precio, cantidad, importe, impuesto, total;
+  // Datos
+  precio = parseFloat(document.getElementById("precio").value);
+  cantidad = parseInt(document.getElementById("cantidad").value);
+  // Proceso
+  total = precio * cantidad;
+  importe = total / 1.18;
+  impuesto = total - importe;
+  // Reporte
+  document.getElementById("importe").value = importe;
+  document.getElementById("impuesto").value = impuesto;
+  document.getElementById("total").value = total;
+};
