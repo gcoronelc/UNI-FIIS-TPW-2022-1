29e225aedece77d88ceece8598c47e2a73182bc4
Gustavo Coronel <gcoronelc@gmail.com>
2022-03-18 10:44:16 -0500

    Semana 5

---
+++ Semana05/Requerimiento1/index.js
@@ -0,0 +1,18 @@
+$(function () {
+  $("#lnkInicio").click(fnLnkInicio);
+  $("#lnkVenta").click(fnLnkVenta);
+  $("#lnkReporte").click(fnLnkReporte);
+});
+
+// Programación del menú
+function fnLnkInicio() {
+  alert("Inicio");
+}
+
+function fnLnkVenta() {
+  alert("Venta");
+}
+
+function fnLnkReporte() {
+  alert("Reporte");
+}
