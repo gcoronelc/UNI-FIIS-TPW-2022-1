a02086c34ffbe1fbb405bdc518fe51f7840f0630
Gustavo Coronel <gcoronelc@gmail.com>
2022-04-01 09:25:40 -0500

    Drivers

---
+++ Semana07/porg01.php
@@ -0,0 +1,11 @@
+<?php
+$serverName = "localhost\sqlexpress, 1433";
+$connectionInfo = array("Database" => "EUREKABANK", "UID" => "sa", "PWD" => "sql", "CharacterSet" => "UTF-8");
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+if ($conn) {
+	echo "Conexión establecida.<br />";
+} else {
+	echo "Conexión no se pudo establecer.<br />";
+	die(print_r(sqlsrv_errors(), true));
+}
